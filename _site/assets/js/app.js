a57b6420b448ce9689631fa2cbc545cc844b985a
Akash Kumar Sharma <vyom@vyomnet.tech>
2022-02-23 14:58:55 +0530

    site upload

---
+++ _site/assets/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Akash Kumar Sharma <vyom@vyom.site>
+ */
+// window.onload = function () {
+//     // contact page animation
+//     var contact_page_animation = bodymovin.loadAnimation({
+//         container: document.getElementById('pg-contact-email-section-image'), // required
+//         path: '{{ site.url }}{{ site.baseurl }}/assets/images/Blogging Minimalistic/Blogging.json', // required
+//         renderer: 'svg', // required
+//         loop: true, // optional
+//         autoplay: true, // optional
+//         name: "Contact page animation", // optional
+//     });
+//     contact_page_animation.goToAndStop();
+// }
